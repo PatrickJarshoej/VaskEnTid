4acaaef19d60235e60c4a896a61fb535de977f29
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-10-07 13:17:32 +0200

    User service og generel skelet

--- VaskEnTid/VaskEnTidLib/Repo/UserRepo.cs
+++ VaskEnTid/VaskEnTidLib/Repo/UserRepo.cs
@@ -7,28 +7,38 @@ using VaskEnTidLib.Model;
 
 namespace VaskEnTidLib.Repo
 {
-    internal class UserRepo
+    internal class UserRepo : IUserRepo
     {
         private List<User> _users;
-        public List<User> GetByDomicileID(int id)
-        {
-            return _users;
-        }
-        //public User GetByID(int id)
-        //{
-        //    return;
-        //}
-        public void Update(User user)
-        {
-
-        }
         public void Add(User user)
         {
-
+            throw new NotImplementedException();
         }
+
         public void DeleteByID(int id)
         {
+            throw new NotImplementedException();
+        }
 
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetByDomicileID(int domicileID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
         }
     }
+
 }
---
+++ VaskEnTid/VaskEnTidLib/Service/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaskEnTidLib.Repo;
+using VaskEnTidLib.Model;
+
+namespace VaskEnTidLib.Service
+{
+    public class UserService
+    {
+        public List<User> GetByDomicileID(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public User GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(User user)
+        {
+
+        }
+        public User CreateUser(string name, string lastName, int domicileID, string email, string phone)
+        {
+            User user = new(name, lastName, domicileID, email, phone);
+            return user;
+        }
+    }
+}
