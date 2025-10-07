17436c24a8d70f6c1646aa960718400ff72fd7d8
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-10-07 12:46:11 +0200

    User repo og interfaces

---
+++ VaskEnTid/VaskEnTidLib/Repo/IUserRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using VaskEnTidLib.Model;
+
+namespace VaskEnTidLib.Repo
+{
+    public interface IUserRepo
+    {
+        public List<User> GetByDomicileID(int domicileID);
+        public User GetByID(int id);
+        public void Update(User user);
+        public void Add(User user);
+        public void DeleteByID(int id);
+    }
+}
