f0407bdd1606cb2d9291f7db5ce6b8524c9388a8
Patrick Bachmann Vanting Jarshøj <paj003@edu.zealand.dk>
2025-10-07 12:28:12 +0200

    Created all Booking Files

---
+++ VaskEnTid/VaskEnTidLib/Service/BookingService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaskEnTidLib.Repo;
+using VaskEnTidLib.Model;
+
+namespace VaskEnTidLib.Service
+{
+    public class BookingService
+    {
+        private IBookingRepo _bookingRepo;
+        public BookingService(IBookingRepo bookingRepo)
+        {
+            _bookingRepo = bookingRepo;
+        }
+
+        public void Create()
+        {
+
+        }
+    }
+}
