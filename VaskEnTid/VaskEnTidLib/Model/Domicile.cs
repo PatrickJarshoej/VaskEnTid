f4ff8edd10aa0a7e9e41a754914606b361b4a9e7
BjarkeZealand <bja001@edu.zealand.dk>
2025-10-07 12:32:29 +0200

    Domicile

---
+++ VaskEnTid/VaskEnTidLib/Model/Domicile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaskEnTidLib.Model
+{
+    
+    internal class Domicile
+    {
+        public int ID {  get; private set; }
+        public List<int> UserIDs { get; private set; }
+
+        public string Roadname { get; private set; }
+        public string Postalcode { get; private set; }
+        public string Floor { get; private set; }
+        public string Door { get; private set; }
+        public string City { get; private set; }
+        public string Region{ get; private set; }
+        public string Country { get; private set; }
+        public double PriceTally { get; private set; }
+
+        public Domicile(string roadname, string postalcode, string floor, string door, string city, string region, string country)
+        {
+            ID = 0;
+            UserIDs = new List<int> ();
+            Roadname = roadname;
+            Postalcode = postalcode;
+            Floor = floor;
+            Door = door;
+            City = city;
+            Region = region;
+            Country = country;
+            PriceTally = 0;
+        }
+    }
+}
