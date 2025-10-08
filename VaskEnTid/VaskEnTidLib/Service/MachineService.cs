e33917e404c1353bad2ea7c3b22ad489f20fa4e6
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-10-08 10:41:08 +0200

    fixed stuffs og lavet services

---
+++ VaskEnTid/VaskEnTidLib/Service/MachineService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaskEnTidLib.Model;
+
+namespace VaskEnTidLib.Service
+{
+    public class MachineService
+    {
+        public void Create(Enum machineType)
+        {
+
+        }
+        public List<Machine> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(Machine machine)
+        {
+
+        }
+        public void DeleteByID(int machineID)
+        {
+
+        }
+        public Machine GetByID(int machineID)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
