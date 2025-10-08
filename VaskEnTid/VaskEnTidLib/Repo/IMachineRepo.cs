f62f6ed1a6f134d4aa00bf6e012b6eb4cb309bea
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-10-08 10:17:01 +0200

    Machine repo og interface er implementeret men mangler stadig at blive udfyldt.

--- VaskEnTid/VaskEnTidLib/Repo/IMachineRepo.cs
+++ VaskEnTid/VaskEnTidLib/Repo/IMachineRepo.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VaskEnTidLib.Model;
 
 namespace VaskEnTidLib.Repo
 {
     public interface IMachineRepo
     {
-
+        public List<Machine> GetAll();
+        public Machine GetByID();
+        public void Add();
+        public void DeleteByID();
+        public void Update();
     }
 }
--- VaskEnTid/VaskEnTidLib/Repo/MachineRepo.cs
+++ VaskEnTid/VaskEnTidLib/Repo/MachineRepo.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VaskEnTidLib.Model;
 
 namespace VaskEnTidLib.Repo
 {
-    internal class MachineRepo
+    internal class MachineRepo : IMachineRepo
     {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Machine> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Machine GetByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
