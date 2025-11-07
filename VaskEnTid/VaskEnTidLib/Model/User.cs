b53411f282ef80d32eed89fc733d0b2bfa6b6d37
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-11-07 09:20:39 +0100

    i forgot, it was alot

--- VaskEnTid/VaskEnTidLib/Model/User.cs
+++ VaskEnTid/VaskEnTidLib/Model/User.cs
@@ -9,27 +9,35 @@ namespace VaskEnTidLib.Model
 {
     public class User
     {
-        public string Name { get; private set; }
+        public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public int UserID { get; private set; }
-        public int DomicileID { get; private set; }
+        public List<int> DomicileID { get; set; }
         public string Email { get; private set; }
         public string Phone { get; private set; }
         public User()
         {
 
         }
-        public User(string name, string lastName, int domicileID, string email, string phone)
+        public User(int userID, string firstname, string lastName, string email, string phone)
         {
-            Name = name;
+            FirstName = firstname;
+            LastName = lastName;
+            UserID = userID;
+            Email = email;
+            Phone = phone;
+        }
+        public User(string firstname, string lastName, List<int> domicileID, string email, string phone)
+        {
+            FirstName = firstname;
             LastName = lastName;
             DomicileID = domicileID;
             Email = email;
             Phone = phone;
         }
-        public User(string name, string lastName, int userID, int domicileID, string email, string phone)
+        public User(string firstname, string lastName, int userID, List<int> domicileID, string email, string phone)
         {
-            Name = name;
+            FirstName = firstname;
             LastName = lastName;
             UserID = userID;
             DomicileID = domicileID;
